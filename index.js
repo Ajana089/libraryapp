389b2a47e926cce222f7baa3ef87b95e03aa5e63
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-09-28 14:54:17 +0400

    inde

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ const cors = require('cors');
 const bcrypt=require('bcrypt');
 var path = require("path");
 app.use(cors());
-// var distDir = __dirname + "/dist/";
-//  app.use(express.static(distDir));
-
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
 
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
-app.set('app','./frontend/src/app'); 
+
 
 const signuprouter=require('./src/routes/singuprouter')
 const bookrouter=require('./src/routes/bookrouter')
